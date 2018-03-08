579387ab921d3004f6e4b3b6ca49986f5dadc710
Rohan Kapur <me@rohankapur.com>
2018-03-08 12:09:52 -0800

    finitial comimit

--- index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Snow from './src/Snow';
-
-ReactDOM.render(<Snow />, document.getElementsByClassName('react-snow-effect')[0]);
-
-exports = Snow;
